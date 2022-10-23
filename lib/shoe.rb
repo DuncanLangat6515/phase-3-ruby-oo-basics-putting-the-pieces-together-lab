a23e765c38f91040f7cb555474d8ffcf49a5f874
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-10-23 11:36:16 +0300

    Completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :color, :size, :material, :condition, :brand
+    def initialize(valueofthebrand)
+        @brand = valueofthebrand
+        
+    end
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
